4fdc3a0f1797d0a7c1148478cebf44a5ebff30ff
Youshin Kim <ro117youshin@gmail.com>
2024-11-20 15:18:04 +0900

    initialize project

---
+++ src/main/java/hello/introduction_to_spring/IntroductionToSpringApplication.java
@@ -0,0 +1,14 @@
+package hello.introduction_to_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IntroductionToSpringApplication.class, args);
+	}
+
+}
